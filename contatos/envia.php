2639d8f033e96478c4d4f703b1156f6c1e3d1dc7
Lincoln <99758281+Liinkiin998@users.noreply.github.com>
2022-04-18 15:03:25 -0300

    sdfsdf

---
+++ contatos/envia.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Arquivo que faz a configuração incial da página.
+ * Por exemplo, conecta-se ao banco de dados.
+ */
+require($_SERVER['DOCUMENT_ROOT'] . '/_config.php');
+
+/**
+ * Processa envio do formulário.
+ */
+
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
+exit;
+
+/**
+ * Variável que define o título desta página.
+ * Essa variável é usada no arquivo "_header.php".
+ * OBS: para a página inicial (index.php) usaremos o 'slogan' do site.
+ *     Referências:
+ *     → https://www.w3schools.com/php/php_variables.asp
+ *     → https://www.php.net/manual/pt_BR/language.variables.basics.php
+ */
+$title = "Faça contato";
+
+/**
+ * Inclui o cabeçalho da página.
+ * A superglobal "$_SERVER['DOCUMENT_ROOT']" retorna o caminho da raiz do site no Windows.
+ * Ex.: C:\xampp\htdocs 
+ *     Referências:
+ *     → https://www.w3schools.com/php/php_includes.asp
+ *     → https://www.php.net/manual/pt_BR/function.include.php
+ *     → https://www.php.net/manual/pt_BR/language.variables.superglobals.php
+ */
+require($_SERVER['DOCUMENT_ROOT'] . '/_header.php');
+
+?>
+
+<section>
+
+
+
+</section>
+
+<aside>
+
+    <h3>Lateral</h3>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, aperiam corporis culpa consequatur
+        iusto.</p>
+
+</aside>
+
+<?php
+
+// Inclui o rodapé da página
+require($_SERVER['DOCUMENT_ROOT'] . '/_footer.php');
